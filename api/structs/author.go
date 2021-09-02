029572abcd3a5110c3bf9cb6c517a38e4903e859
Marcos Travasso <marcos.travasso42@gmail.com>
2021-09-02 10:37:02 -0300

    Added author SQL statement function and tests

--- api/structs/author.go
+++ api/structs/author.go
@@ -1,6 +1,34 @@
 package structs
 
+import (
+	"errors"
+	"fmt"
+)
+
 type Author struct {
 	ID     int    `json:"id"`
 	Person Person `json:"person"`
+}
+
+func (a Author) SQLStatement(statementType string) (string, error) {
+	sqlStatement := ""
+
+	switch statementType {
+	case "INSERT":
+		if a.Person.ID == 0 {
+			return "", errors.New("author has no person ID")
+		}
+		sqlStatement += fmt.Sprintf("INSERT INTO Autores(pessoa) values (\"%d\")", a.Person.ID)
+	case "DELETE":
+		if a.ID == 0 {
+			return "", errors.New("author has no ID")
+		}
+		sqlStatement += fmt.Sprintf("DELETE FROM Autores WHERE idAutor = \"%d\"", a.ID)
+	case "SELECT":
+		if a.ID == 0 {
+			return "", errors.New("author has no ID")
+		}
+		sqlStatement += fmt.Sprintf("SELECT * FROM Autores WHERE idAutor = \"%d\"", a.ID)
+	}
+	return sqlStatement, nil
 }
---
+++ api/structs/author_test.go
@@ -0,0 +1,55 @@
+package structs
+
+import "testing"
+
+func TestAuthor_AuthorStatement(t *testing.T) {
+	tests := []struct {
+		name             string
+		args             Author
+		wantedStatements map[string]string
+	}{
+		{name: "One author",
+			args: Author{
+				ID:     1,
+				Person: Person{ID: 1},
+			},
+			wantedStatements: map[string]string{
+				"INSERT": "INSERT INTO Autores(pessoa) values (\"1\")",
+				"DELETE": "DELETE FROM Autores WHERE idAutor = \"1\"",
+				"SELECT": "SELECT * FROM Autores WHERE idAutor = \"1\"",
+			},
+		},
+		{name: "Empty author ID",
+			args: Author{
+				ID:     0,
+				Person: Person{ID: 1},
+			},
+			wantedStatements: map[string]string{
+				"INSERT": "INSERT INTO Autores(pessoa) values (\"1\")",
+				"DELETE": "",
+				"SELECT": "",
+			},
+		},
+		{name: "Empty person ID",
+			args: Author{
+				ID:     1,
+				Person: Person{ID: 0},
+			},
+			wantedStatements: map[string]string{
+				"INSERT": "",
+				"DELETE": "DELETE FROM Autores WHERE idAutor = \"1\"",
+				"SELECT": "SELECT * FROM Autores WHERE idAutor = \"1\"",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for statementType, wanted := range tt.wantedStatements {
+				if got, _ := tt.args.SQLStatement(statementType); got != wanted {
+					t.Errorf("%s statement got = %v, expect = %v", statementType, got, wanted)
+				}
+			}
+		})
+	}
+}

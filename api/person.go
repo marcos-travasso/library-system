baf6651e1bb3aff704ce51a32fba979e477ff5fa
Marcos Travasso <marcos.travasso42@gmail.com>
2021-09-01 22:17:14 -0300

    Separated person struct file

---
+++ api/person.go
@@ -0,0 +1,41 @@
+package api
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Person struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Gender   string `json:"gender"`
+	Birthday string `json:"birthday"`
+}
+
+func (p Person) SQLStatement(statementType string) (string, error) {
+	sqlStatement := ""
+
+	switch statementType {
+	case "INSERT":
+		if p.Name == "" {
+			return "", errors.New("person has no name")
+		}
+		sqlStatement += fmt.Sprintf("INSERT INTO Pessoas(Nome, Genero, Nascimento) values (\"%s\", \"%s\", \"%s\")", p.Name, p.Gender, p.Birthday)
+	case "UPDATE":
+		if p.ID == 0 {
+			return "", errors.New("person has no ID")
+		}
+		sqlStatement += fmt.Sprintf("UPDATE Pessoas SET nome=\"%s\", genero=\"%s\", nascimento=\"%s\" WHERE idPessoa = \"%d\"", p.Name, p.Gender, p.Birthday, p.ID)
+	case "DELETE":
+		if p.ID == 0 {
+			return "", errors.New("person has no ID")
+		}
+		sqlStatement += fmt.Sprintf("DELETE FROM Pessoas WHERE idPessoa = \"%d\"", p.ID)
+	case "SELECT":
+		if p.ID == 0 {
+			return "", errors.New("person has no ID")
+		}
+		sqlStatement += fmt.Sprintf("SELECT * FROM Pessoas WHERE idPessoa = \"%d\"", p.ID)
+	}
+	return sqlStatement, nil
+}
--- api/structsAPI.go
+++ api/structsAPI.go
@@ -1,10 +1,5 @@
 package api
 
-import (
-	"errors"
-	"fmt"
-)
-
 type Book struct {
 	ID     int     `json:"id"`
 	Year   int     `json:"year"`
@@ -27,13 +22,6 @@ type User struct {
 	CPF         string  `json:"cpf"`
 	Email       string  `json:"email"`
 	Address     Address `json:"address"`
-}
-
-type Person struct {
-	ID       int    `json:"id"`
-	Name     string `json:"name"`
-	Gender   string `json:"gender"`
-	Birthday string `json:"birthday"`
 }
 
 type Genre struct {
@@ -62,32 +50,4 @@ type Lending struct {
 
 type Devolution struct {
 	Date string `json:"date"`
-}
-
-func (p Person) SQLStatement(statementType string) (string, error) {
-	sqlStatement := ""
-
-	switch statementType {
-	case "INSERT":
-		if p.Name == "" {
-			return "", errors.New("person has no name")
-		}
-		sqlStatement += fmt.Sprintf("INSERT INTO Pessoas(Nome, Genero, Nascimento) values (\"%s\", \"%s\", \"%s\")", p.Name, p.Gender, p.Birthday)
-	case "UPDATE":
-		if p.ID == 0 {
-			return "", errors.New("person has no ID")
-		}
-		sqlStatement += fmt.Sprintf("UPDATE Pessoas SET nome=\"%s\", genero=\"%s\", nascimento=\"%s\" WHERE idPessoa = \"%d\"", p.Name, p.Gender, p.Birthday, p.ID)
-	case "DELETE":
-		if p.ID == 0 {
-			return "", errors.New("person has no ID")
-		}
-		sqlStatement += fmt.Sprintf("DELETE FROM Pessoas WHERE idPessoa = \"%d\"", p.ID)
-	case "SELECT":
-		if p.ID == 0 {
-			return "", errors.New("person has no ID")
-		}
-		sqlStatement += fmt.Sprintf("SELECT * FROM Pessoas WHERE idPessoa = \"%d\"", p.ID)
-	}
-	return sqlStatement, nil
 }

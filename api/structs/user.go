b875b090beebcaa8b237b69b1b8055020805ee7d
Marcos Travasso <marcos.travasso42@gmail.com>
2021-09-03 10:19:36 -0300

    Add user SQL statement function and tests

--- api/structs/user.go
+++ api/structs/user.go
@@ -1,5 +1,10 @@
 package structs
 
+import (
+	"errors"
+	"fmt"
+)
+
 type User struct {
 	ID          int     `json:"id"`
 	Person      Person  `json:"person"`
@@ -18,4 +23,37 @@ type Address struct {
 	Neighborhood string `json:"neighborhood"`
 	Street       string `json:"street"`
 	Complement   string `json:"complement"`
+}
+
+func (u User) SQLStatement(statementType string) (string, error) {
+	sqlStatement := ""
+
+	switch statementType {
+	case "INSERT":
+		if u.Person.ID == 0 {
+			return "", errors.New("user has no person ID")
+		}
+		sqlStatement += fmt.Sprintf("INSERT INTO Usuarios(pessoa, celular, telefone, endereco, cpf, email, criacao) values (\"%d\", \"%s\", \"%s\", \"%d\", \"%s\", \"%s\", date('now'))", u.Person.ID, u.CellNumber, u.PhoneNumber, u.Address.ID, u.CPF, u.Email)
+	case "UPDATE":
+		if u.ID == 0 {
+			return "", errors.New("user has no ID")
+		}
+		if u.Person.ID == 0 {
+			return "", errors.New("user has no person ID")
+		}
+		sqlStatement += fmt.Sprintf("UPDATE Usuarios SET pessoa=\"%d\", celular=\"%s\", telefone=\"%s\", endereco=\"%d\", cpf=\"%s\", email=\"%s\" WHERE idUsuario = \"%d\"", u.Person.ID, u.CellNumber, u.PhoneNumber, u.Address.ID, u.CPF, u.Email, u.ID)
+	case "DELETE":
+		if u.ID == 0 {
+			return "", errors.New("user has no ID")
+		}
+		sqlStatement += fmt.Sprintf("DELETE FROM Usuarios WHERE idUsuario = \"%d\"", u.ID)
+	case "SELECT":
+		if u.ID == 0 {
+			return "", errors.New("genre has no ID")
+		}
+		sqlStatement += fmt.Sprintf("SELECT * FROM Usuarios WHERE idUsuario = \"%d\"", u.ID)
+	default:
+		return "", errors.New("invalid statement type")
+	}
+	return sqlStatement, nil
 }
---
+++ api/structs/user_test.go
@@ -0,0 +1,113 @@
+package structs
+
+import (
+	"testing"
+)
+
+func TestUser_UserStatement(t *testing.T) {
+	tests := []struct {
+		name             string
+		args             User
+		wantedStatements map[string]string
+	}{
+		{name: "One user",
+			args: User{
+				ID: 1,
+				Person: Person{
+					ID: 2,
+				},
+				CellNumber:  "12345678910",
+				PhoneNumber: "9876543210",
+				CPF:         "12345678910",
+				Email:       "testmail@mail.test",
+				Address:     Address{ID: 3},
+			},
+			wantedStatements: map[string]string{
+				"INSERT": "INSERT INTO Usuarios(pessoa, celular, telefone, endereco, cpf, email, criacao) values (\"2\", \"12345678910\", \"9876543210\", \"3\", \"12345678910\", \"testmail@mail.test\", date('now'))",
+				"UPDATE": "UPDATE Usuarios SET pessoa=\"2\", celular=\"12345678910\", telefone=\"9876543210\", endereco=\"3\", cpf=\"12345678910\", email=\"testmail@mail.test\" WHERE idUsuario = \"1\"",
+				"DELETE": "DELETE FROM Usuarios WHERE idUsuario = \"1\"",
+				"SELECT": "SELECT * FROM Usuarios WHERE idUsuario = \"1\"",
+				"TEST":   "",
+			},
+		},
+		{name: "Empty person ID",
+			args: User{
+				ID: 1,
+				Person: Person{
+					ID: 0,
+				},
+				CellNumber:  "12345678910",
+				PhoneNumber: "9876543210",
+				CPF:         "12345678910",
+				Email:       "testmail@mail.test",
+				Address:     Address{ID: 3},
+			},
+			wantedStatements: map[string]string{
+				"INSERT": "",
+				"UPDATE": "",
+				"DELETE": "DELETE FROM Usuarios WHERE idUsuario = \"1\"",
+				"SELECT": "SELECT * FROM Usuarios WHERE idUsuario = \"1\"",
+				"TEST":   "",
+			},
+		},
+		{name: "Empty user ID",
+			args: User{
+				ID: 0,
+				Person: Person{
+					ID: 2,
+				},
+				CellNumber:  "12345678910",
+				PhoneNumber: "9876543210",
+				CPF:         "12345678910",
+				Email:       "testmail@mail.test",
+				Address:     Address{ID: 3},
+			},
+			wantedStatements: map[string]string{
+				"INSERT": "INSERT INTO Usuarios(pessoa, celular, telefone, endereco, cpf, email, criacao) values (\"2\", \"12345678910\", \"9876543210\", \"3\", \"12345678910\", \"testmail@mail.test\", date('now'))",
+				"UPDATE": "",
+				"DELETE": "",
+				"SELECT": "",
+				"TEST":   "",
+			},
+		},
+		{name: "Empty fields",
+			args: User{
+				ID: 0,
+				Person: Person{
+					ID: 0,
+				},
+				CellNumber:  "",
+				PhoneNumber: "",
+				CPF:         "",
+				Email:       "",
+				Address:     Address{},
+			},
+			wantedStatements: map[string]string{
+				"INSERT": "",
+				"UPDATE": "",
+				"DELETE": "",
+				"SELECT": "",
+				"TEST":   "",
+			},
+		},
+		{name: "Empty struct",
+			wantedStatements: map[string]string{
+				"INSERT": "",
+				"UPDATE": "",
+				"DELETE": "",
+				"SELECT": "",
+				"TEST":   "",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for statementType, wanted := range tt.wantedStatements {
+				if got, _ := tt.args.SQLStatement(statementType); got != wanted {
+					t.Errorf("%s statement got = %v, expect = %v", statementType, got, wanted)
+				}
+			}
+		})
+	}
+}

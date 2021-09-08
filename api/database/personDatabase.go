d9d13e303ac56e7a8c6d7b784ff156845ce447ab
Marcos Travasso <marcos.travasso42@gmail.com>
2021-09-08 12:40:20 -0300

    Add delete person function

--- api/database/personDatabase.go
+++ api/database/personDatabase.go
@@ -27,6 +27,23 @@ func (dbDir Database) insertPerson(p entity) (int, error) {
 	return id, nil
 }
 
+func (dbDir Database) deletePerson(p entity) error {
+	var db = initializeDatabase(dbDir)
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Printf("Error to close database: %v", err)
+		}
+	}(db)
+
+	err := sendStatement(p, "DELETE", db)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func getLastPersonID(db *sql.DB) (int, error) {
 	rows, err := db.Query("SELECT idPessoa from Pessoas ORDER BY idPessoa DESC LIMIT 1")
 	if err != nil {

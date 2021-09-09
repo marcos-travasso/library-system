a21c7f370ef6846315b0107ac4acb871e76fa842
Marcos Travasso <marcos.travasso42@gmail.com>
2021-09-09 13:10:12 -0300

    Add select books function and tests

--- api/database/bookDatabase.go
+++ api/database/bookDatabase.go
@@ -93,3 +93,53 @@ func (dbDir Database) SelectBook(b structs.Book) (structs.Book, error) {
 
 	return book, nil
 }
+
+func (dbDir Database) SelectBooks() ([]structs.Book, error) {
+	var db = initializeDatabase(dbDir)
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Printf("Error to close database: %v", err)
+		}
+	}(db)
+
+	bookCount, err := dbDir.countRows("Livros")
+	if err != nil {
+		log.Printf("Fail to receive book count: %s", err)
+		return nil, err
+	}
+
+	books := make([]structs.Book, bookCount, bookCount)
+
+	rows, err := db.Query("SELECT idLivro, titulo, ano, paginas, autor, idPessoa, nome, genero, nascimento FROM (Livros INNER JOIN Autores A on Livros.autor = A.idAutor) INNER JOIN Pessoas on pessoa = Pessoas.idPessoa")
+	if err != nil {
+		log.Printf("Fail to query books: %s", err)
+		return nil, err
+	}
+
+	for i := 0; rows.Next(); i++ {
+		err = rows.Scan(&books[i].ID, &books[i].Title, &books[i].Year, &books[i].Pages, &books[i].Author.ID, &books[i].Author.Person.ID, &books[i].Author.Person.Name, &books[i].Author.Person.Gender, &books[i].Author.Person.Birthday)
+		if err != nil {
+			log.Printf("Fail to receive books: %s", err)
+			return nil, err
+		}
+	}
+
+	for i := range books {
+		rows, err = db.Query(books[i].LinkSQLStatement("SELECT"))
+		if err != nil {
+			log.Printf("Fail to query book genre: %s", err)
+			return nil, err
+		}
+
+		for rows.Next() {
+			err = rows.Scan(&books[i].Genre.ID, &books[i].Genre.Name)
+			if err != nil {
+				log.Printf("Fail to receive genre: %s", err)
+				return nil, err
+			}
+		}
+	}
+
+	return books, nil
+}
--- api/database/bookDatabase_test.go
+++ api/database/bookDatabase_test.go
@@ -195,3 +195,110 @@ func TestDatabase_SelectBook(t *testing.T) {
 		})
 	}
 }
+
+func TestDatabase_SelectBooks(t *testing.T) {
+	{
+		tests := []struct {
+			name string
+			args structs.Book
+		}{
+
+			{
+				name: "First book",
+				args: structs.Book{
+					ID:    1,
+					Year:  1977,
+					Pages: 90,
+					Title: "A hora da estrela",
+					Author: structs.Author{
+						ID: 1,
+						Person: structs.Person{
+							ID:       1,
+							Name:     "Clarice Lispector",
+							Gender:   "F",
+							Birthday: "10/12/1920",
+						},
+					},
+					Genre: structs.Genre{
+						ID:   1,
+						Name: "Romance",
+					},
+				},
+			},
+			{
+				name: "Second book",
+				args: structs.Book{
+					ID:    2,
+					Year:  1881,
+					Pages: 368,
+					Title: "Memórias, Póstumas de Brás Cubas",
+					Author: structs.Author{
+						ID: 2,
+						Person: structs.Person{
+							ID:       2,
+							Name:     "Machado de Assis",
+							Gender:   "M",
+							Birthday: "21/6/1839",
+						},
+					},
+					Genre: structs.Genre{
+						ID:   1,
+						Name: "Romance",
+					},
+				},
+			},
+			{
+				name: "Third book",
+				args: structs.Book{
+					ID:    3,
+					Year:  1882,
+					Pages: 96,
+					Title: "O Alienista",
+					Author: structs.Author{
+						ID: 2,
+						Person: structs.Person{
+							ID:       2,
+							Name:     "Machado de Assis",
+							Gender:   "M",
+							Birthday: "21/6/1839",
+						},
+					},
+					Genre: structs.Genre{
+						ID:   2,
+						Name: "Tale",
+					},
+				},
+			},
+		}
+
+		dbDir := Database{Dir: "./temp/test_booksSelect.db"}
+		err := dbDir.clearDatabase()
+		if err != nil {
+			t.Error(err)
+		}
+
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				_, err := dbDir.InsertBook(tt.args)
+				if err != nil {
+					t.Error(err)
+				}
+			})
+		}
+
+		books := make([]structs.Book, len(tests), len(tests))
+		books, err = dbDir.SelectBooks()
+		if err != nil {
+			t.Error(err)
+		}
+
+		for i := range tests {
+			if tests[i].args != books[i] {
+				gotJSON, _ := json.Marshal(books[i])
+				wantJSON, _ := json.Marshal(tests[i].args)
+
+				t.Errorf("Select books got = %v, want %v", string(gotJSON), string(wantJSON))
+			}
+		}
+	}
+}

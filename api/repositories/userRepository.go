35ca816f3c0ac56c529aa0b7421dd526851cfa8b
Marcos Travasso <marcos.travasso42@gmail.com>
2022-04-04 19:16:18 -0300

    :sparkles: Add user service basic functions

--- api/repositories/userRepository.go
+++ api/repositories/userRepository.go
@@ -1,65 +1,41 @@
 package repositories
 
 import (
+	"database/sql"
 	"github.com/marcos-travasso/library-system/models"
 	"log"
 )
 
-func InsertUser(u models.User) (int64, error) {
-	db := initializeDatabase()
-	defer db.Close()
-
-	//TODO maybe pass this insertperson and address to service
-	personID, err := InsertPerson(u.Person)
-	if err != nil {
-		log.Println("insert person error: " + err.Error())
-		return 0, err
-	}
-	u.Person.ID = int(personID)
-
-	addressID, err := InsertAddress(u.Address)
-	if err != nil {
-		log.Println("insert address error: " + err.Error())
-		return 0, err
-	}
-	u.Address.ID = int(addressID)
-
+func InsertUser(db *sql.DB, u *models.User) (err error) {
 	result, err := db.Exec("INSERT INTO Usuarios(pessoa, celular, telefone, endereco, cpf, email, criacao) values (?, ?, ?, ?, ?, ?, date('now'))", u.Person.ID, u.CellNumber, u.PhoneNumber, u.Address.ID, u.CPF, u.Email)
 	if err != nil {
 		log.Println("insert user error: " + err.Error())
-		return 0, err
+		return err
 	}
 
-	return result.LastInsertId()
+	u.ID, err = result.LastInsertId()
+	return
 }
 
-func SelectUser(u models.User) (models.User, error) {
-	db := initializeDatabase()
-	defer db.Close()
-
-	var user models.User
-
+func SelectUser(db *sql.DB, user *models.User) (err error) {
 	//TODO check if responsible is necessary
-	row := db.QueryRow("SELECT idUsuario, celular, telefone, cpf, email, criacao, idPessoa, nome, genero, nascimento, idEndereco, cep, cidade, bairro, rua, numero, complemento FROM ((Usuarios INNER JOIN Pessoas ON Pessoas.idPessoa = Usuarios.pessoa) INNER JOIN Enderecos ON Usuarios.endereco = Enderecos.idEndereco) WHERE idUsuario =?", u.ID)
+	row := db.QueryRow("SELECT * FROM Usuarios WHERE idUsuario = ?", user.ID)
 	if row.Err() != nil {
 		log.Println("select user error: " + row.Err().Error())
-		return user, row.Err()
+		return row.Err()
 	}
 
-	err := row.Scan(&user.ID, &user.CellNumber, &user.PhoneNumber, &user.CPF, &user.Email, &user.CreationDate, &user.Person.ID, &user.Person.Name, &user.Person.Gender, &user.Person.Birthday, &user.Address.ID, &user.Address.CEP, &user.Address.City, &user.Address.Neighborhood, &user.Address.Street, &user.Address.Number, &user.Address.Complement)
+	err = row.Scan(&user.ID, &user.Person.ID, &user.CellNumber, &user.PhoneNumber, &user.Address.ID, &user.CPF, &user.Email, &user.Responsible.ID, &user.CreationDate)
 	if err != nil {
 		log.Println("scan user error: " + err.Error())
-		return user, err
+		return
 	}
 
 	user.CreationDate = user.CreationDate[:10]
-	return user, nil
+	return
 }
 
-func SelectUsers() ([]models.User, error) {
-	db := initializeDatabase()
-	defer db.Close()
-
+func SelectUsers(db *sql.DB) ([]models.User, error) {
 	users := make([]models.User, 0)
 
 	rows, err := db.Query("SELECT idUsuario, celular, telefone, cpf, email, criacao, idPessoa, nome, genero, nascimento, idEndereco, cep, cidade, bairro, rua, numero, complemento FROM ((Usuarios INNER JOIN Pessoas ON Pessoas.idPessoa = Usuarios.pessoa) INNER JOIN Enderecos ON Usuarios.endereco = Enderecos.idEndereco)")
@@ -84,12 +60,12 @@ func SelectUsers() ([]models.User, error) {
 	return users, nil
 }
 
-func DeleteUser(u models.User) error {
+func DeleteUser(db *sql.DB, u *models.User) (err error) {
 	//TODO
 	return nil
 }
 
-func UpdateUser(u models.User) error {
+func UpdateUser(db *sql.DB, u *models.User) (err error) {
 	//TODO
 	return nil
 }
